cc9237870bc8f2d7a40c9886cd53af3bc993f439
kazuumi-hori <kazuumi.hori@spacemarket.co.jp>
2017-05-22 17:47:58 +0900

    2枚目のスクリーン

--- app/config/route.js
+++ app/config/route.js
@@ -1,7 +1,11 @@
-import HomeScreen from '../screen/HomeScreen.js';
+import HomeScreen from '../screen/HomeScreen';
+import SearchScreen from '../screen/SearchScreen';
 
 export const route = {
 	Home: {
 		screen: HomeScreen
+	},
+	Search: {
+		screen: SearchScreen
 	},
 }
